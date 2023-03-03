74283f0d2eccb7728e20a2202987e0aff887d145
Kaushal S <117001590+kaushal16s@users.noreply.github.com>
2023-03-03 18:11:08 +0530

    Add files via upload

---
+++ components/Layout.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Head from 'next/head';
+import { Navbar } from '.';
+import { Footer } from '.';
+
+const Layout = ({ children }) => {
+  return (
+    <div className='layout'>
+      <Head>
+        <title>A&S Beverages</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className='main-container'>
+        { children }
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  )
+}
+
+export default Layout
